ce9adbfdbdced44513e7e57a63c866f1a71f23c0
wonderotter <gouk32@naver.com>
2021-01-21 15:05:56 +0900

    이벤트 마우스 이미지 변경

--- section5/js/delegation.js
+++ section5/js/delegation.js
@@ -1,4 +1,5 @@
 const menu = document.querySelector('.menu');
+let beforeValue = null;
 
 function clickHandler(e) {
   let target = e.target;
@@ -9,6 +10,17 @@ function clickHandler(e) {
       return;
     }
   }
+
+  const btnValue = target.dataset.value;
+
+  const body = document.getElementsByTagName('body')[0];
+  body.classList.add(`cursor_${btnValue}`);
+
+  if (beforeValue !== btnValue) {
+    body.classList.remove(`cursor_${beforeValue}`);
+  }
+
+  beforeValue = btnValue;
 }
 
 menu.addEventListener('click', clickHandler);
