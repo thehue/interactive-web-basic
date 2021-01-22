a5080ad36be58f9596a354b09987629dc459a2e4
wonderotter <gouk32@naver.com>
2021-01-22 15:16:26 +0900

    스크롤 위치에 따라 오브젝트 조작

---
+++ section8/js/01.js
@@ -0,0 +1,19 @@
+(function () {
+  const outputElem = document.querySelector('.output');
+  const ilbuniElem = document.querySelector('.ilbuni');
+
+  function showValue() {
+    let posY = ilbuniElem.getBoundingClientRect().top;
+    outputElem.innerHTML = posY;
+
+    if (posY < window.innerHeight * 0.2) {
+      ilbuniElem.classList.add('zoom');
+    } else {
+      ilbuniElem.classList.remove('zoom');
+    }
+  }
+
+  window.addEventListener('scroll', function () {
+    showValue();
+  });
+})();
