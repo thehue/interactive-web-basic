3facf42e877409ade67a9be5177a510b08f97d3f
HeeYou Jang <gouk32@naver.com>
2021-06-12 23:13:19 +0900

    resizeHanlder 추가

--- section11/my_work/js/wall3d.js
+++ section11/my_work/js/wall3d.js
@@ -1,10 +1,18 @@
 // 즉시 실행 함수
 (function () {
   const houseElm = document.querySelector(".house");
-  let maxScrollValue = document.body.offsetHeight - window.innerHeight;
+  let maxScrollValue = 0;
+
+  function resizeHandler() {
+    maxScrollValue = document.body.offsetHeight - window.innerHeight;
+  }
 
   window.addEventListener("scroll", function () {
-    const zMove = (this.pageYOffset / maxScrollValue) * 980;
-    houseElm.style.transform = `translateZ(${-490 + zMove}vw)`;
+    const zMove = (this.pageYOffset / maxScrollValue) * 980 - 490;
+    houseElm.style.transform = `translateZ(${zMove}vw)`;
   });
+
+  window.addEventListener("resize", resizeHandler);
+
+  resizeHandler();
 })();
