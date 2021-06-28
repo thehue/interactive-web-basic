53cc9c17d3a0fc7bad8c38f5a36b69375052292b
HeeYou Jang <gouk32@naver.com>
2021-06-28 16:04:33 +0900

    스크롤바 만들기

--- section11/my_work/js/wall3d.js
+++ section11/my_work/js/wall3d.js
@@ -1,6 +1,7 @@
 // 즉시 실행 함수
 (function () {
   const houseElm = document.querySelector(".house");
+  const barElem = document.querySelector(".progress-bar");
   let maxScrollValue = 0;
 
   function resizeHandler() {
@@ -8,8 +9,12 @@
   }
 
   window.addEventListener("scroll", function () {
-    const zMove = (this.pageYOffset / maxScrollValue) * 980 - 490;
+    const scrollPer = this.pageYOffset / maxScrollValue;
+    const zMove = scrollPer * 980 - 490;
     houseElm.style.transform = `translateZ(${zMove}vw)`;
+
+    //progress bar
+    barElem.style.width = `${scrollPer * 100}%`;
   });
 
   window.addEventListener("resize", resizeHandler);
